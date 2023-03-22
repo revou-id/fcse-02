110740a4997944ef8d04e9fb62a82d5c57e96d20
Naufaldi Rafif S <naufaldi.rafif@efishery.com>
2023-03-22 21:14:19 +0700

    feat: add calculate js

---
+++ js/index.js
@@ -0,0 +1,39 @@
+// Javascript Variable
+
+var x = 5;
+let y = 6;
+const z = x + y;
+document.getElementById("demo").innerHTML = "The value of z is: " + z;
+
+// Javascript Conditional
+const time = new Date().getHours();
+// z = 100;
+let greeting;
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+document.getElementById("conditional").innerHTML = greeting;
+
+// Looping
+const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+let text = "";
+for (let i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
+document.getElementById("looping").innerHTML = text;
+// console.log("text", text);
+//javascript function
+myFunction();
+
+function myFunction() {
+  let carName = "Volvo";
+  document.getElementById("function-one").innerHTML =
+    typeof carName + " " + carName;
+}
+
+document.getElementById("function-two").innerHTML = typeof carName;
